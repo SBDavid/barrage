cb291aaef43a248dd2492d12b2696ae6b1e2bae8
SBDavid <billl89@126.com>
2017-07-23 16:38:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ function barrage(ele) {
 
 var defalutConfig = {
     bulletHeight: 30,
-    fireInterval: 500,
+    fireInterval: 200,
     v: 50,
-    max: 50
+    max: 50,
+    isDebug: false
+}
+
+function log(isDebug, ...para) {
+    if (isDebug) {
+        console.info(para);
+    }
 }
 
 function initTrack(config, tracks, height) {
@@ -18,7 +25,8 @@ function initTrack(config, tracks, height) {
             isBusy: false
         });
     }
-    console.info('初始化弹道数量: ', trackAmount);
+
+    log(config.isDebug, `初始化弹道数量: ${trackAmount}`);
 }
 
 function createBullet(config, bulletInfo, trackInfo, rootWidth) {
@@ -37,46 +45,65 @@ function mountBullet(ele, bullet) {
     ele.appendChild(bullet);
 }
 
-function fireBullet(config, domBullet, trackInfo, bulletRunningAmount, group, width, tweenPool) {
+function loadBullet(config, domBullet, trackInfo, group, width, tweenPool) {
+    tweenPool.set(domBullet.id, {
+        domBullet: domBullet,
+        trackInfo: trackInfo,
+        width: width,
+        steps: { left: 0 }
+    });
+    var step1 = createStep1(config, domBullet, trackInfo, group, width, tweenPool, tweenPool.get(domBullet.id).steps);
+    step1.start();
+    tweenPool.get(domBullet.id).step = step1;
+}
+
+function resumeBullet(config, domBullet, trackInfo, group, width, tweenPool, steps) {
+    if (steps.left <= domBullet.offsetWidth) {
+        var step1 = createStep1(config, domBullet, trackInfo, group, width, tweenPool, steps);
+        step1.start();
+        tweenPool.get(domBullet.id).step = step1;
+    } else {
+        var step2 = createStep2(config, domBullet, trackInfo, group, width, tweenPool, steps);
+        step2.start();
+        tweenPool.get(domBullet.id).step = step2;
+    }
+}
+
+function createStep1(config, domBullet, trackInfo, group, width, tweenPool, steps) {
     var bulletWidth = domBullet.offsetWidth;
-    bulletRunningAmount.amount++;
-    var step = { left: 0 };
-    var step1 = new TWEEN.Tween(step, group)
-        .to({ left: bulletWidth }, parseInt(bulletWidth / config.v * 1000))
+    var step1 = new TWEEN.Tween(steps, group)
+        .to({ left: bulletWidth }, parseInt((bulletWidth - steps.left) / config.v * 1000))
         .easing(TWEEN.Easing.Linear.None)
         .onUpdate(function () {
-            $(domBullet).css('transform', `translateX(-${step.left}px) translateZ(0px)`);
+            $(domBullet).css('transform', `translateX(-${steps.left}px) translateZ(0px)`);
         })
         .onComplete(function () {
-            console.info('弹道进入空闲状态：', trackInfo);
+            log(config.isDebug, '弹道进入空闲状态');
             trackInfo.isBusy = false;
+            var step2 = createStep2(config, domBullet, trackInfo, group, width, tweenPool, steps);
+            step2.start();
+            tweenPool.get(domBullet.id).step = step2;
         })
-    var step2 = new TWEEN.Tween(step, group)
-        .to({ left: bulletWidth + width }, parseInt(bulletWidth + width / config.v * 1000))
-        .onStart(function() {
-            tweenPool.set(domBullet.id, {
-                step: step2
-            })
-        })
+   return step1;
+}
+
+function createStep2(config, domBullet, trackInfo, group, width, tweenPool, steps) {
+    var bulletWidth = domBullet.offsetWidth;
+    var step2 = new TWEEN.Tween(steps, group)
+        .to({ left: bulletWidth + width }, parseInt((bulletWidth + width - steps.left) / config.v * 1000))
         .easing(TWEEN.Easing.Linear.None)
         .onUpdate(function () {
-            $(domBullet).css('transform', `translateX(-${step.left}px) translateZ(0px)`);
+            $(domBullet).css('transform', `translateX(-${steps.left}px) translateZ(0px)`);
         })
         .onComplete(function () {
-            console.info('step2 is Complete');
-            var deleteIndex;
-            bulletRunningAmount.amount--;
+            log(config.isDebug, 'step2 is Complete');
             $(domBullet).remove();
             tweenPool.delete(domBullet.id);
-            if (bulletRunningAmount.amount === 0) {
+            if (tweenPool.length === 0) {
                 this.isRunning = false;
             }
         })
-    step1.chain(step2);
-    step1.start();
-    tweenPool.set(domBullet.id, {
-        step: step1
-    });
+    return step2;
 }
 
 barrage.prototype.animate = function (time) {
@@ -89,29 +116,30 @@ barrage.prototype.animate = function (time) {
 barrage.prototype.init = function (config) {
 
     this.config = Object.assign(defalutConfig, config || {});
-    console.info('初始化参数: ', this.config);
+    log(this.config.isDebug, '弹幕初始化');
 
     this.bulletId = 0;
     this.bulletPool = [];
-    this.bulletRunningAmount = { amount: 0 };
     this.tracks = [];
     this.fireTimer = null;
     this.tweenGroup = new TWEEN.Group();
     this.tweenPool = new Map();
     this.isRunning = false;
+    this.status = 'init';
 
     initTrack(this.config, this.tracks, this.root.offsetHeight);
 }
 
 barrage.prototype.fire = function () {
     if (this.bulletPool.length === 0) {
-        console.info('没有可用子弹，停止发射');
+        log(this.config.isDebug, '没有可用子弹，停止发射');
         clearInterval(this.fireTimer);
+        this.status = 'idle';
         return;
     }
 
-    if (this.bulletRunningAmount.amount >= this.config.max) {
-        console.info('弹幕数量达到上限');
+    if (this.tweenPool.length >= this.config.max) {
+        log(this.config.isDebug, '弹幕数量达到上限');
         return;
     }
 
@@ -119,35 +147,37 @@ barrage.prototype.fire = function () {
         return item.isBusy === false;
     })
     if (idleTracks.length === 0) {
-        console.info('没有可用的弹道');
+        log(this.config.isDebug, '没有可用的弹道');
         return;
     }
     var targetTrack = idleTracks[Math.floor(Math.random() * idleTracks.length)];
     targetTrack.isBusy = true;
 
-    console.info('找到空闲弹道: ', targetTrack);
+    log(this.config.isDebug, `找到空闲弹道 id: ${targetTrack.id}`);
 
     var bullet = this.bulletPool.shift();
     var domBullet = createBullet(this.config, bullet, targetTrack, this.root.offsetWidth);
     mountBullet(this.root, domBullet);
-    fireBullet(
+    loadBullet(
         this.config,
         domBullet,
         targetTrack,
-        this.bulletRunningAmount,
         this.tweenGroup,
         this.root.offsetWidth,
         this.tweenPool
     );
-    console.info('fire bullet: ', bullet);
+    log(this.config.isDebug, `fire bullet msg: ${bullet.msg}`);
 }
 
 barrage.prototype.start = function () {
-    this.isRunning = true;
-    this.fireTimer = setInterval(function () {
-        this.fire();
-    }.bind(this), this.config.fireInterval);
-    this.animate();
+    if (this.status && this.status === 'init') {
+        this.isRunning = true;
+        this.status = 'running';
+        this.fireTimer = setInterval(function () {
+            this.fire();
+        }.bind(this), this.config.fireInterval);
+        this.animate();
+    }
 }
 
 barrage.prototype.load = function (bullets) {
@@ -156,22 +186,50 @@ barrage.prototype.load = function (bullets) {
         this.bulletId++;
     }.bind(this));
     this.bulletPool = this.bulletPool.concat(bullets);
-    console.info('装载子弹：', this.bulletPool);
+    log(this.config.isDebug, `装载子弹，子弹数量：${this.bulletPool.length}`);
+    if (this.status && this.status === 'idle') {
+        this.isRunning = true;
+        this.status = 'running';
+        this.fireTimer = setInterval(function () {
+            this.fire();
+        }.bind(this), this.config.fireInterval);
+        this.animate();
+    }
 }
 
 barrage.prototype.pause = function (bullets) {
-    clearInterval(this.fireTimer);
-    this.tweenPool.forEach(function (element) {
-        element.step.stop();
-    });
-    this.isRunning = false;
-    console.info('暂定弹幕滚动');
+    if (this.status && this.status !== 'paused') {
+        this.status = 'paused';
+        clearInterval(this.fireTimer);
+        this.tweenPool.forEach(function (element) {
+            element.step.stop();
+        });
+        this.isRunning = false;
+        log(this.config.isDebug, '暂定弹幕滚动');
+    }
 }
 
-barrage.prototype.resume = function (bullets) {
-    this.tweenPool.forEach(function (element) {
-        element.step.start();
-    });
-    this.start();
-    console.info('恢复弹幕滚动');
+barrage.prototype.play = function (bullets) {
+    if (this.status && this.status !== 'running') {
+        this.status = 'running';
+        this.tweenPool.forEach(function (element) {
+            resumeBullet(
+                this.config,
+                element.domBullet,
+                element.trackInfo,
+                this.tweenGroup,
+                this.root.offsetWidth,
+                this.tweenPool,
+                element.steps
+            )
+        }, this);
+        
+        this.isRunning = true;
+        this.fireTimer = setInterval(function () {
+            this.fire();
+        }.bind(this), this.config.fireInterval);
+        this.animate();
+
+        log(this.config.isDebug, '恢复弹幕滚动');
+    }
 }
